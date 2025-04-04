7525f785af77a2c9fa771b5ff1d3b8f2ee551de9
RickeyAuger <augerrickey@gmail.com>
2025-04-03 21:29:37 -0400

    splitnodes italic update

--- src/splitnodes.py
+++ src/splitnodes.py
@@ -142,4 +142,5 @@ def text_to_textnodes(text):
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
     return nodes
