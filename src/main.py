c6a3a94e662bc70fbf079d4bda7a024e4b86c71c
RickeyAuger <augerrickey@gmail.com>
2025-04-05 00:06:51 -0400

    implemented more pages and contact

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from website import *
+import os
 
 
 
@@ -7,7 +8,21 @@ from website import *
 
 def main():
     copy_static("static", "public")
+    
     generate_page("content/index.md", "template.html", "public/index.html")
+    generate_page("content/contact/index.md", "template.html", "public/contact/index.html")
+    
+    
+    for file_name in os.listdir('content/blog'):
+        full_content_path = f"content/blog/{file_name}/index.md"
+        full_public_path = f"public/blog/{file_name}/index.html"
+        
+     
+        generate_page(full_content_path, "template.html", full_public_path )
+
+    
+    
+   
     
 
 if __name__ == "__main__":
